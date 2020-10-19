8772056326b7537bf96df357d49b7744581fea79
Yatin <yatin.soni@invenco.com>
2020-10-19 23:23:57 +1300

    Commit changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as BR } from 'react-router-dom';
+import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BR>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </BR>,
+  document.getElementById('root'));
